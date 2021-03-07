c2229ed84e07ebddf076110083017d0d6cd958df
Alex Roux <arouxandrey@gmail.com>
2021-03-07 14:05:31 +0100

    Update ip logging function

--- middleware/requestLogger.js
+++ middleware/requestLogger.js
@@ -4,14 +4,18 @@ const colors = require("colors");
 // RequestLogger
 // Logs informations about the request
 const requestLogger = function(req, res, next) {
+    
     let reqTime = new Date().getTime();
     let reqUrl = req.url, logString;
     let isResource = false;
     if(!reqUrl.includes(".")) {
-        logString = "New request : " + "Hostname: ".info + req.hostname + " ║ " + "URL: ".info + req.method + req.url;
+        logString = "New request : " + "IP: ".info + req.ip + " ║ " + "URL: ".info + req.method + req.url;
+
+        //const ip = ;
+        //console.log(ip);
     } else {
         isResource = true;
-        logString = "New request : ".grey + "Hostname: ".grey + req.hostname.grey + " ║ ".grey + "URL: ".grey + req.method.grey + req.url.grey;
+        logString = "New request : ".grey + "IP: ".grey + req.ip + " ║ ".grey + "URL: ".grey + req.method.grey + req.url.grey;
     }
     if(utils.config.logResources || !isResource) {
         utils.log(logString, 1); // Call log function with info
